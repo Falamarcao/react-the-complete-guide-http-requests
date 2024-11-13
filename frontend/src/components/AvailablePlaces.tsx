354230d87e7bb2063583413f09cb9a8551f5cf4e
falamarcao <falamarcao@gmail.com>
2024-11-13 02:41:37 -0300

    Handling HTTP Errors

--- frontend/src/components/AvailablePlaces.tsx
+++ frontend/src/components/AvailablePlaces.tsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from 'react';
 import { Place } from '../models/Place.ts';
 
 import Places from './Places';
+import ErrorPage from './ErrorPage.tsx';
 
 interface AvailablePlacesProps {
   onSelectPlace: (place: Place) => void;
 }
 
 const fetchPlaces = async (): Promise<Place[]> => {
-  const response = await fetch('http://localhost:3000/places');
+  const response = await fetch('http://localhost:3000/places$'); // wrong url to trigger error
 
   if (response.status === 200) {
     return (await response.json()).places as Place[];
+  } else if (!response.ok) {
+    throw new Error('Failed to fetch places');
   }
 
   return [];
@@ -22,13 +25,23 @@ export default function AvailablePlaces({
 }: AvailablePlacesProps) {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [availablePlaces, setAvailablePlaces] = useState<Place[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   useEffect(() => {
-    fetchPlaces().then((places: Place[]) => {
-      setAvailablePlaces(places);
-      setIsLoading(false);
-    });
+    fetchPlaces()
+      .then((places: Place[]) => {
+        setAvailablePlaces(places);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
+
+  if (errorMessage)
+    return <ErrorPage title="An error occured" message={errorMessage} />;
 
   return (
     <Places
---
+++ frontend/src/components/ErrorPage.tsx
@@ -0,0 +1,25 @@
+interface ErrorPageProps {
+  title: string;
+  message: string;
+  onConfirm?: () => void;
+}
+
+export default function ErrorPage({
+  title,
+  message,
+  onConfirm,
+}: ErrorPageProps) {
+  return (
+    <div className="error">
+      <h2>{title}</h2>
+      <p>{message}</p>
+      {onConfirm && (
+        <div id="confirmation-actions">
+          <button onClick={onConfirm} className="button">
+            Okay
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
